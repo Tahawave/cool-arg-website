0b425e32db7693a0b73d87031a23bdff3c325c9c
Tahawave <167723648+Tahawave@users.noreply.github.com>
2024-04-27 17:04:27 -0400

    Update Decapsulation.js

--- Decapsulation.js
+++ Decapsulation.js
@@ -1,1 +1,10 @@
-
+var password = "james_manipulation_destruction_532";
+function passcheck() {
+if (document.getElementById('manipulation').value != password) {
+alert('Wrong Password, Try Again');
+return false;
+}
+if (document.getElementById('manipulation').value == password) {
+alert('Correct Password, click OK to enter into the webpage, director!');
+}
+}
